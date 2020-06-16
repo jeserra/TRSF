4c327aba7ee5741d944e856eb2d78db4734fe5f5
josermac <joser_isi@hotmail.com>
2020-06-16 17:50:24 -0500

    Samples session 10

---
+++ sample22/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace sample22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string teststring = "Soy la rosa que marchito el olvido";
+
+            Console.WriteLine(teststring.Length);
+            Console.WriteLine("Primera R --- ");
+            Console.WriteLine(teststring.IndexOf("r"));
+            Console.WriteLine(teststring.Contains("rosa"));
+
+            string clonstring = (String) teststring.Clone();
+            Console.WriteLine(clonstring) ;
+
+            Console.WriteLine( teststring.Remove(28, 6));
+            Console.WriteLine(teststring.Insert(4, "juana, "));
+            Console.WriteLine( teststring);
+
+            Console.WriteLine(teststring.LastIndexOf("e"));
+            Console.WriteLine(teststring.ToUpper());
+            Console.WriteLine(teststring.ToLower());
+            Console.WriteLine (teststring.Replace("rosa", "tuna"));
+
+            Console.WriteLine(teststring.Substring(1, 20));
+            var listacaracteres = teststring.Split("r");
+            foreach(var item in listacaracteres)
+                Console.WriteLine(item);
+
+
+
+        }
+    }
+}
---
+++ sample23/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace sample23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Soy la flor silvestre que marchito el olvido");
+            sb.Append(" y vivo en este eterno pesar");
+
+
+            Console.WriteLine(sb.ToString());
+
+            sb.Replace("flor", "rosa");
+
+            Console.WriteLine(sb.Length);
+            Console.WriteLine (sb.ToString());
+
+            sb.Remove(sb.Length - 10, 10);
+            Console.WriteLine (sb.ToString());
+        }
+    }
+}
