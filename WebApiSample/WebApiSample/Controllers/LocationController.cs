e96b7f1671cb1e0c5444fd38efb90a92b9e305f8
josermac <joser_isi@hotmail.com>
2020-09-29 17:18:59 -0500

    add web api simple sample

---
+++ WebApiSample/WebApiSample/Controllers/LocationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApiSample.Models;
+
+namespace WebApiSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LocationController : ControllerBase
+    {
+        private readonly ILogger<LocationController> _logger;
+
+        public LocationController(ILogger<LocationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<LocationDto> GetLocations()
+        {
+            List<LocationDto> list = new List<LocationDto>()
+            {
+                new LocationDto () { Name = "Zapopan", Latitud = 23, Longitud = 23 },
+                new LocationDto () { Name = "rio Grande", Latitud = 22, Longitud = 99},
+                new LocationDto () { Name = "Sombrerete", Latitud = 45, Longitud = 99}
+            };
+
+            return list;
+        }
+    }
+}
